487e92f2f1f26b0789b4ab36c3840b178b4164e9
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-09-28 20:39:01 +0100

    Test #update_quality for ordinary items

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,12 +1,22 @@
-require File.join(File.dirname(__FILE__), 'gilded_rose')
+require 'gilded_rose'
 
 describe GildedRose do
 
   describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+    context 'updating an ordinary item' do
+      context 'before the sell by date has passed' do
+        ordinary_item = Item.new("toothpaste", 10, 20)
+        gilded_rose = GildedRose.new([ordinary_item])
+
+        it 'decreases the number of days in which to sell the item' do
+          gilded_rose.update_quality
+          expect(ordinary_item.sell_in). to be(9)
+        end
+
+        it 'decreases the quality of the item' do
+          expect(ordinary_item.quality). to be(19)
+        end
+      end
     end
   end
 
