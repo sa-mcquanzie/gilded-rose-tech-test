7b7f74e7272518f741055cb3980be1cbd3fee07b
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-09-30 12:48:58 +0100

    Give up on ItemMethods module, move methods in to updateable

--- lib/updateable.rb
+++ lib/updateable.rb
@@ -38,4 +38,26 @@ module Updateable
   def age
     @sell_in -= 1
   end
+
+  def within_date?
+    @sell_in.positive?
+  end
+
+  def past_date?
+    !within_date?
+  end
+
+  def appreciate(amount)
+    @quality += amount
+    @quality = 50 if self.quality > 50
+  end
+
+  def depreciate(amount)
+    @quality -= amount
+    @quality = 0 if @quality.negative?
+  end
+
+  def make_quality_zero
+    @quality = 0
+  end
 end
--- spec/unit/updateable_spec.rb
+++ spec/unit/updateable_spec.rb
@@ -4,21 +4,62 @@ describe Updateable do
   class FakeItem
     attr_accessor :category, :sell_in, :quality
 
-    def initialize(category, sell_in, quality)
-      @category, @sell_in, @quality = category, sell_in, quality
+    def initialize
+      @category, @sell_in, @quality = nil, nil, nil
     end
   end
+
+  let(:item) { FakeItem.new.extend(subject) }
 
   context 'when another object extends it' do    
     describe '#update' do
       it 'decreases the sell_in time by 1' do
-        item = FakeItem.new(:ordinary, 5, 2).extend(subject)
-
-        allow(item).to receive(:within_date?)
-        allow(item).to receive(:depreciate)
+        item.category = :ordinary
+        item.sell_in = 5
+        item.quality = 2
 
         item.update
         expect(item.sell_in).to eq(4)
+      end
+
+      context 'for ordinary items' do
+        before { item.category = :ordinary }
+
+        it 'reduces the quality by 1 before the sell_in days run out' do
+          item.sell_in = 5
+          item.quality = 2
+
+          item.update
+          expect(item.quality).to eq(1)
+        end
+
+        it 'reduces the quality by 2 after the sell_in days run out' do
+          item.sell_in = 0
+          item.quality = 4
+
+          item.update
+          expect(item.quality).to eq(2)
+        end
+      end
+
+      context 'for items that age well' do
+        before { item.category = :ages_well }
+
+        it 'increases the quality by 1 before the sell_in days run out' do
+          item.sell_in = 5
+          item.quality = 2
+
+          item.update
+          expect(item.quality).to eq(3)
+        end
+
+        # it 'reduces the quality by 2 after the sell_in days run out' do
+        #   item.sell_in = 0
+        #   item.quality = 4
+
+        #   item.update
+        #   expect(item.quality).to eq(2)
+        # end
       end
     end
   end
